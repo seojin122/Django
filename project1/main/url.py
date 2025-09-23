e4ffdfe1701fb2b64e16e8c32e24fcc4f10249d3
이서진 <akflsdlek23@naver.com>
2025-09-23 10:31:28 +0900

    서브모듈 Django 수정

--- project1/main/url.py
+++ project1/main/url.py
@@ -1,3 +1,8 @@
 from django.urls import path
 from . import views
-urlpatterns = [ path('', views.index, name='index'),]
+
+urlpatterns = [ 
+    path('', views.index, name='index'),
+    path('hello', views.hello, name='hello'),
+    path('api_example',views.api_example,name='api_example'),
+]
--- project1/main/views.py
+++ project1/main/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse, Http404, HttpResponseNotAllowed
+from django.views import View
 
-# Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    if request.method!="GET":
+        raise HttpResponseNotAllowed()
+    context = {
+        "title":"myTitle",
+        "content":"hello"
+    }
+    return render(request,'main/index.html', context)
+
+def hello(request):
+    return HttpResponse("<h1>Hello, World!</h1><h2>Well...</h2>")
+
+def toGoogle(request):
+    return redirect("https://google.com")
+
+def not_found(request):
+    raise Http404("Sorry... no page here.")
+
+def api_example(request):
+    if request.method=="GET":
+        data = {
+        "message": "Hello, this is a JSON response",
+        "status": "success"
+        }
+        return JsonResponse(data)
+    elif request.method=="DELETE":
+        return HttpResponse('delete done')
+    else:
+        raise HttpResponseNotAllowed()
