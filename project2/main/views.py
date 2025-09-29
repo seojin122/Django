549e1d2a756b6cbe162def602c5f65c0863c6eb0
이서진 <akflsdlek23@naver.com>
2025-09-29 19:01:15 +0900

    Models + Forms

--- project2/main/views.py
+++ project2/main/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import *
+from . import models
+from . import forms
 
 def home(request):
     return HttpResponse("Hello from main!")
+
+
+def index(request):
+    students = models.Student.objects.all()
+    return render(request,'main/index.html',{'students':students})
+def add(request):
+    if request.method == 'GET':
+        form = forms.StudentForm()
+        return render(request,'main/add.html',{'form':form})
+    if request.method == 'POST':
+        form = forms.StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+    else:
+        return HttpResponseNotAllowed('Method not allowed')
